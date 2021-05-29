8d7b109268d9eb3c67c45ed04aaa41b53387ae68
Mahmud <mahmudfrzl@mail.ru>
2021-05-29 19:34:25 +0400

    Updated

--- northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/SuccessDataResult.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/SuccessDataResult.java
@@ -19,3 +19,4 @@ public class SuccessDataResult<T> extends DataResult<T> {
 		
 	}
 }
+ 
--- northwind/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Entity
 @Table(name="products")
@@ -23,8 +25,8 @@ public class Product {
 	    @Column(name="product_id")
 		private int id;
 	    
-	    @Column(name="category_id")
-		private int categoryId;
+	    //@Column(name="category_id")
+		//private int categoryId;
 	    
 	    @Column(name="product_name")
 		private String productName;
@@ -38,8 +40,8 @@ public class Product {
 	    @Column(name="quantity_per_unit")
 		private String quantityPerUnit;
 	    
-
-		
-		
-		
+	    @ManyToOne()
+	    @JoinColumn(name="category_id")
+	    private Category category;
+	
 }
